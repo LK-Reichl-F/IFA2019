76a71be85ccb733e1ce206cb51dfe1fb6a1652f4
Florian Reichl <florian@reichl.net>
2020-12-14 08:28:45 +0100

    Typo

--- Gruppe1/SAPDatentypen/HTMLJSSpezifikation.js
+++ Gruppe1/SAPDatentypen/HTMLJSSpezifikation.js
@@ -1,4 +1,4 @@
 // Eure Aufgabe:
-// Nehnmt die Spezifikationen, wie sie in SAPDatentypen.ts beschrieben sind und versucht,
+// Nehmt die Spezifikationen, wie sie in SAPDatentypen.ts beschrieben sind und versucht,
 // diese Spezifikationen in HTML und JavaScript hier abzuprüfen:
 
