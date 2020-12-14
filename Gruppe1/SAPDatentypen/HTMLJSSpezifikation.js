87a64e2d901c48dbb8fc3bda1dbccd1db2c017be
Florian Reichl <florian@reichl.net>
2020-12-14 09:29:29 +0100

    Feld meinAlter mit Überprüfung eingebaut.

--- Gruppe1/SAPDatentypen/HTMLJSSpezifikation.js
+++ Gruppe1/SAPDatentypen/HTMLJSSpezifikation.js
@@ -2,3 +2,22 @@
 // Nehmt die Spezifikationen, wie sie in SAPDatentypen.ts beschrieben sind und versucht,
 // diese Spezifikationen in HTML und JavaScript hier abzuprüfen:
 
+// Diese Funktion ausführen, wenn alle Dateien geladen sind:
+window.onload = function () {
+    const meinAlter = document.getElementById("meinAlter");
+    meinAlter.addEventListener("input", meinAlterInput);
+}
+
+function meinAlterInput(ereignis) {
+    const meinAlter = ereignis.target;      // const meinAlter = document.getElementById("meinAlter");
+    const zustand = meinAlter.validity;
+    if (zustand.rangeUnderflow) {
+        meinAlter.setCustomValidity("Alter zu klein.");
+    } else if (zustand.rangeOverflow) {
+        meinAlter.setCustomValidity("Alter zu groß.");
+    } else {
+        // Wichtig: Wenn alles ok, unbedingt Fehler löschen:
+        meinAlter.setCustomValidity("");
+    }
+    meinAlter.reportValidity();
+}
