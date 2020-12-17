f9be40914dc57f5a863b81f028de495ffa8a23f0
Florian Reichl <florian@reichl.net>
2020-12-17 12:06:10 +0100

    Observer ausprogrammiert

--- Gruppe2/Observer/Observer.js
+++ Gruppe2/Observer/Observer.js
@@ -1,14 +1,28 @@
 // Übung:
 // Teste selbst das Observer Pattern.
 // Orientiere Dich dazu am Klassendiagramm:
-// Schreibe zuerst das Observer-Interface
-// Es hat nur die Methode update(), die nichts zurückgibt.
 // Danach programmiere die Klasse Subject.
 // Sie hat die Methoden attach und detach, die
 // einen Observer speichern.
 // Verwende dazu einen Set. Siehe https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Set
 // Die Methode notify() geht mit einer for-of-Schleife https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Statements/for...of
 // durch die Werte im Set durch und ruft für jedes Objekt die update-Funktion auf.
+class Subject {
+    constructor() {
+        this.listeVonObservern = new Set();
+    }
+    attach(observer) {
+        this.listeVonObservern.add(observer);
+    }
+    detach(observer) {
+        this.listeVonObservern.delete(observer);
+    }
+    notify() {
+        for (let observer of this.listeVonObservern) {
+            observer.update();
+        }
+    }
+}
 // Die Klasse ConcreteSubject erbt von Subject.
 // Die Methode setState(daten:string) speichert
 // einen String ab und ruft danach this.notify() auf,
@@ -16,6 +30,15 @@
 // bekommen.
 // Die Methode getState():string gibt den gespeicherten
 // string zurück.
+class ConcreteSubject extends Subject {
+    setState(neuerWert) {
+        this.state = neuerWert;
+        this.notify();
+    }
+    getState() {
+        return this.state;
+    }
+}
 // Die Klasse ConcreteObserver erfüllt das interface
 // Observer.
 // Deshalb hat sie eine Methode update().
@@ -25,4 +48,19 @@
 // ConcreteObserver das ConcreteSubject abgespeichert sein.
 // Außerdem muss sich der ConcreteObserver beim
 // ConcreteSubject mit attach anmelden.
+class ConcreteObserver {
+    constructor(subject) {
+        this.subject = subject;
+    }
+    update() {
+        console.log(this.subject.getState());
+    }
+}
+const meinSubject = new ConcreteSubject();
+const meinObserver = new ConcreteObserver(meinSubject);
+meinSubject.attach(meinObserver);
+meinSubject.setState("Hello world");
+const zweiterObserver = new ConcreteObserver(meinSubject);
+meinSubject.attach(zweiterObserver);
+meinSubject.setState("Zweite Nachricht");
 //# sourceMappingURL=Observer.js.map
--- Gruppe2/Observer/Observer.ts
+++ Gruppe2/Observer/Observer.ts
@@ -5,12 +5,31 @@
 // Schreibe zuerst das Observer-Interface
 // Es hat nur die Methode update(), die nichts zurückgibt.
 
+interface Observer {
+    update(): void;
+}
+
 // Danach programmiere die Klasse Subject.
 // Sie hat die Methoden attach und detach, die
 // einen Observer speichern.
 // Verwende dazu einen Set. Siehe https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/Set
 // Die Methode notify() geht mit einer for-of-Schleife https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Statements/for...of
 // durch die Werte im Set durch und ruft für jedes Objekt die update-Funktion auf.
+
+class Subject {
+    private listeVonObservern = new Set<Observer>();
+    public attach(observer: Observer): void {
+        this.listeVonObservern.add(observer);
+    }
+    public detach(observer: Observer): void {
+        this.listeVonObservern.delete(observer);
+    }
+    public notify(): void {
+        for (let observer of this.listeVonObservern) {
+            observer.update();
+        }
+    }
+}
 
 // Die Klasse ConcreteSubject erbt von Subject.
 // Die Methode setState(daten:string) speichert
@@ -19,6 +38,17 @@
 // bekommen.
 // Die Methode getState():string gibt den gespeicherten
 // string zurück.
+
+class ConcreteSubject extends Subject {
+    private state: string;
+    public setState(neuerWert: string) {
+        this.state = neuerWert;
+        this.notify();
+    }
+    public getState(): string {
+        return this.state;
+    }
+}
 
 // Die Klasse ConcreteObserver erfüllt das interface
 // Observer.
@@ -30,3 +60,20 @@
 // Außerdem muss sich der ConcreteObserver beim
 // ConcreteSubject mit attach anmelden.
 
+class ConcreteObserver implements Observer {
+    private subject: ConcreteSubject;
+    public constructor(subject: ConcreteSubject) {
+        this.subject = subject;
+    }
+    public update() {
+        console.log(this.subject.getState());
+    }
+}
+
+const meinSubject = new ConcreteSubject();
+const meinObserver = new ConcreteObserver(meinSubject);
+meinSubject.attach(meinObserver);
+meinSubject.setState("Hello world");
+const zweiterObserver = new ConcreteObserver(meinSubject);
+meinSubject.attach(zweiterObserver);
+meinSubject.setState("Zweite Nachricht");
